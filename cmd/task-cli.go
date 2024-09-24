ae2882a7cac1cf1ca7cd004ac36d3cae4b9494b0
qaraku <1925527396@qq.com>
2024-09-25 00:31:20 +0800

    feat(task-cli): implement loadTasks function and Execute function in task-cli
    This commit introduces two core functions to the `task-cli` application, `loadTasks` and `Execute`,
    which together establish the basic framework for the task-tracker command-line interface.

--- cmd/task-cli.go
+++ cmd/task-cli.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"task-cli/model"
 )
 
@@ -50,5 +53,73 @@ func listTasks(status string) {
 }
 
 func Execute() int {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("Usage: task-cli [command] [args...]")
+		return 1
+	}
+
+	command := args[0]
+	switch command {
+	case "add":
+		if len(args) < 2 {
+			fmt.Println("Usage: task-cli add [description]")
+			return 1
+		}
+		addTask(strings.Join(args[1:], " "))
+	case "update":
+		if len(args) < 3 {
+			fmt.Println("Usage: task-cli update [id] [new description]")
+			return 1
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Invaild task ID: %v\n", err)
+			return 1
+		}
+		updateTask(id, strings.Join(args[2:], " "))
+	case "delete":
+		if len(args) < 2 {
+			fmt.Println("Usage: task-cli delete [id]")
+			return 1
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Invalid task ID: %v\n", err)
+			return 1
+		}
+		deleteTask(id)
+	case "mark-in-progress":
+		if len(args) < 2 {
+			fmt.Println("Usage: task-cli mark-in-progress [id]")
+			return 1
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Invalid task ID: %v\n", err)
+			return 1
+		}
+		markTaskStatus(id, "in-progress")
+	case "mark-done":
+		if len(args) < 2 {
+			fmt.Println("Usage: task-cli mark-done [id]")
+			return 1
+		}
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Invalid task ID: %v\n", err)
+			return 1
+		}
+		markTaskStatus(id, "done")
+	case "list":
+		if len(args) > 1 {
+			listTasks(args[1])
+		} else {
+			listTasks("")
+		}
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		return 1
+	}
 	return 0
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "task-cli/cmd"
 
+func main() {
+	cmd.Execute()
 }
